203ca0339eede15b00b3fe33b9ac5c82d6e760a7
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-01-05 16:13:18 -0500

    init files added

---
+++ README.md
@@ -0,0 +1,42 @@
+# Die Class Constructor
+
+![Github license](https://img.shields.io/badge/mit-blue.svg)
+
+Licensing: [mit](https://choosealicense.com/licenses/mit/)
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation-requirements)
+- [Usage](#application-usage)
+- [License](#licensing-information)
+- [Contributions](#contributions)
+- [Tests](#tests-commands)
+- [Video Link](#link-to-video-instructions)
+- [Screenshot](#screenshot)
+- [Questions](#questions)
+
+## Description
+A backend die rolling application, designed for practicing with objects and probability logic using NodeJS.
+
+## Development
+Collaborate to develop an app that creates objects with probability functions that can be tested and parsed.
+
+## Installation Requirements
+npm i inquirer@8.2.4
+
+*We are using the require() syntax in inquirer@8.2.4 instead of the ES6 import syntax.*
+
+## Application Usage
+Utilize the command line to create instances of dice, roll them, and then view the statistics of those roles!
+
+## Contributions
+Eric Keeton
+
+*Insert Dad Name Here*
+
+## Test Commands
+node index.js
+
+## Questions
+For Questions, contact me at EMK2473@gmail.com or visit My Github: [EMK2473](https://github.com/EMK2473)
---
+++ index.js
@@ -0,0 +1,71 @@
+const inquirer = require('inquirer');
+const DSix = require('./dSix');
+const DEight = require('./dEight');
+const DTwenty = require('./dTwenty');
+
+async function rollDice() {
+  try {
+    // Create a list of die types
+    const dieTypes = ['D6', 'D8', 'D20'];
+
+    // Prompt to select the type of die
+    const answers = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'dieType',
+        message: 'Select the type of die you want to roll:',
+        choices: dieTypes,
+      },
+    ]);
+
+    // Get the selected die type
+    const selectedDieType = answers.dieType;
+
+    // Prompt to ask how many of the selected die to roll
+    const secondAnswers = await inquirer.prompt([
+      {
+        type: 'number',
+        name: 'numberOfDice',
+        message: `How many ${selectedDieType}s do you want to roll?`,
+        validate: (value) => {
+          return value > 0 ? true : 'Please enter a valid number greater than 0.';
+        },
+      },
+    ]);
+
+    // Get the number of dice to roll
+    const numberOfDice = secondAnswers.numberOfDice;
+
+    // Roll the selected die type
+    let selectedDie;
+    switch (selectedDieType) {
+      case 'D6':
+        selectedDie = new DSix();
+        break;
+      case 'D8':
+        selectedDie = new DEight();
+        break;
+      case 'D20':
+        selectedDie = new DTwenty();
+        break;
+      default:
+        console.log('Invalid die type');
+        return;
+    }
+
+    // Roll the die the specified number of times
+    for (let i = 0; i < numberOfDice; i++) {
+      selectedDie.roll();
+      console.log(`${selectedDieType} Rolls: ${selectedDie.dieRollValueA}, ${selectedDie.dieRollValueB}`);
+      console.log(`Sum: ${selectedDie.createSumRoll()}`);
+      console.log(`Min: ${selectedDie.createMinRoll()}`);
+      console.log(`Max: ${selectedDie.createMaxRoll()}`);
+      console.log('------------------------');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+// Call the async function
+rollDice();
